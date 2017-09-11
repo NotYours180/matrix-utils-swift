42d585056278876db982cd0b125a6d2f44d1f0d8
Leonardo Vernaza <leo0307vb@hotmail.com>
2017-09-11 17:43:51 -0500

    add start of month and end of month functions to Date helper

--- Sources/Date+Helper.swift
+++ Sources/Date+Helper.swift
@@ -93,4 +93,12 @@ extension Date {
             return (0, "")
         }
     }
+
+    public func startOfMonth() -> Date {
+        return Calendar.current.date(from: Calendar.current.dateComponents([.year, .month], from: Calendar.current.startOfDay(for: self)))!
+    }
+
+    public func endOfMonth() -> Date {
+        return Calendar.current.date(byAdding: DateComponents(month: 1, day: -1), to: self.startOfMonth())!
+    }
 }
